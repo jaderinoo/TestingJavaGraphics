b1bcbc6e45ca7925b8f3af7cf1f3c58a19d88054
Jad El-Khatib <jaderinoo@gmail.com>
2019-11-12 02:05:46 -0500

    Created an external console

---
+++ TestingGraphics/src/Frame.java
@@ -0,0 +1,38 @@
+  
+import java.awt.Color;
+import java.awt.Font;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+class Console {
+	final JFrame frame = new JFrame();
+	
+	public Console() {
+		JTextArea textArea = new JTextArea(24, 80);
+		textArea.setBackground(Color.LIGHT_GRAY);
+		textArea.setForeground(Color.black);
+		textArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 15));
+		System.setOut(new PrintStream(new OutputStream() {
+    	@Override
+    	public void write(int b) throws IOException {
+    		textArea.append(String.valueOf((char) b));
+    	}
+    }));
+    	frame.add(textArea);
+  }
+	public void init() {
+		frame.pack();
+		frame.setVisible(true);
+		frame.setAlwaysOnTop(true);
+
+	}
+	public JFrame getFrame() {
+		return frame;
+	}
+}
--- TestingGraphics/src/Main.java
+++ TestingGraphics/src/Main.java
@@ -1,4 +1,12 @@
 
 public class Main {
 	//Initial Commit
+	public static void main(String[] args) {
+		Console console = new Console();
+	    console.init();
+	    Main launcher2 = new Main();
+	    
+	    System.out.println("Hello");
+	}
+
 }
