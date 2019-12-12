6bb3f1082d316cb4aebb5675586cb08850f12409
jaderinoo <jaderinoo@gmail.com>
2019-12-11 19:11:08 -0500

    Implemented reactive key presses

--- TestingGraphics/src/Main.java
+++ TestingGraphics/src/Main.java
@@ -18,19 +18,7 @@ public class Main {
     static int col = 0;
     
     public static void main(String[] args) throws IOException, InterruptedException {
-
-        Scanner sc = new Scanner(System.in); 
-
     	paint();
-
-    	while(row <= 30) {
-    		System.out.println("row + ?");
-    		row = row + sc.nextInt();
-    	}
-
-    
-
-
     }
 
 
@@ -41,7 +29,6 @@ public class Main {
         JPanel pane = new JPanel() {
         	
             @Override
-            
             protected void paintComponent(Graphics g) {
             	super.removeAll();
                 super.paintComponent(g);
@@ -52,44 +39,4 @@ public class Main {
 
         frame.add(pane);
     }
-	
-	
-	public static void grabInput(Frame frame, int type) throws InterruptedException {
-		
-		//If type == 0, Grab int
-		//If type == 1, Grab String
-		if(type == 0){
-		    System.out.println("Int Selection: ");
-
-		}else if(type == 1) {
-		    System.out.println("String Selection: ");
-		}
-		
-		//Wait for user input
-		do{
-		    Thread.sleep(750);
-		}while(frame.text == null);
-		
-		//After grabbing the selection, send it to user and reset text to null for later
-		if(type == 0){
-		    System.out.println("Username: " + frame.getUserIntInput());
-
-		}else if(type == 1) {
-		    System.out.println("Username: " + frame.getUserStringInput());
-		}
-		frame.text = null;
-	}
-	
-	public static void printTest(Frame frame, int turnNumber, int userInput) throws InterruptedException {
-		frame.enemies--;
-		
-		for (int i = 0; i < userInput; i++) {
-			Thread.sleep(500);
-			turnNumber++;
-			frame.setTurnNumber(turnNumber);
-		}
-	    frame.setEnemies((frame.enemies--));
-		frame.setMapArea();
-	}
-
 }
--- TestingGraphics/src/MapSpace.java
+++ TestingGraphics/src/MapSpace.java
@@ -1,12 +1,86 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.IOException;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.WindowConstants;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
 
 public class MapSpace {
+    /**
+     * @wbp.parser.entryPoint
+     */
     static JFrame mapSpace() {
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(200, 200);
+        
+        JButton send = new JButton("Enter");
+        frame.getContentPane().add(send, BorderLayout.SOUTH);
         frame.setVisible(true);
-        return frame;
+
+        //Sets send as the default key
+        frame.getRootPane().setDefaultButton(send);
+        
+        JPanel pane = new JPanel();
+        
+        frame.getContentPane().add(pane);
+        
+        pane.addKeyListener(new KeyListener() {
+
+            @Override
+            public void keyTyped(KeyEvent e) {}
+
+            @Override
+            public void keyReleased(KeyEvent e) {}
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+            	
+            	if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    System.out.println("Up");
+                    Main.row++;
+                    System.out.println(Main.row);
+                    try {
+						Main.paint();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+                }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                	System.out.println("Down");
+                	Main.row--;
+                	System.out.println(Main.row);
+                	try {
+						Main.paint();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+                }
+            }
+        });
+        
+        pane.setFocusable(true);
+        pane.requestFocusInWindow();
+        
+        
+    
+        //Send button listener
+        send.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	System.out.println("Enter");
+            }
+        });
+		return frame;
     }
+    
+    
+    
 }
